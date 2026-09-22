9b888c3fbaba977e9aef9084d6100ea2429c5991
jeremiah solomon <oyofaith802@gmail.com>
2026-09-21 18:58:06 -0700

    Enable Creator Video generation button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6853,7 +6853,7 @@ function handleCountryChange(
                   )}
                 </div>
 
-                <button className="generate-button" style={{ width: "100%", marginTop: "30px" }} disabled={creatorRenderLoading || creatorSaving} onClick={handleCreatorRender}>{creatorRenderLoading ? "Rendering video..." : " Generate Video"}</button>
+                <button type="button" className="generate-button" style={{ width: "100%", marginTop: "30px", cursor: "pointer" }} onClick={handleCreatorRender}>{creatorRenderLoading ? "Rendering video..." : " Generate Video"}</button>
                 {creatorRenderError && <div className="error" style={{ marginTop: "15px" }}>{creatorRenderError}</div>}
               </section>
 
@@ -6880,6 +6880,7 @@ function handleCountryChange(
 
 
 export default App;
+
 
 
 
